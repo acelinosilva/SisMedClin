6920d906dc89a14bba11e3b26ecb23af27be6a79
omagoobscuro <celticfolkmetal@hotmail.com>
2019-09-07 21:24:15 -0300

    debug

--- sistema/settings.py
+++ sistema/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8wyr=%+hmc2cx8@bt)q_r@-&i!bccm!q-om2v&6+bp=qt(0hd%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.localhost', '.herokuapp.com']
 
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = ['127.0.0.1', '.localhost', '.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
-    
     'material.admin',
     'django.contrib.admin',
     'django.contrib.auth',
