4f7fbacdc145ddfef00aff194b609d3031fff340
omagoobscuro <celticfolkmetal@hotmail.com>
2019-09-08 10:29:45 -0300

    erro

--- sistema/settings.py
+++ sistema/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import dj_database_url
-import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -126,4 +126,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static") ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+import django_heroku
 django_heroku.settings(locals())
