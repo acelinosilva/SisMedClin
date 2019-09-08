37b5ffcb8fe70db3e9e936bf288282759e1db27b
omagoobscuro <celticfolkmetal@hotmail.com>
2019-09-08 11:44:37 -0300

    settings

--- sistema/settings.py
+++ sistema/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8wyr=%+hmc2cx8@bt)q_r@-&i!bccm!q-om2v&6+bp=qt(0hd%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,8 +124,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static") ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile','static')
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import django_heroku
 django_heroku.settings(locals())
